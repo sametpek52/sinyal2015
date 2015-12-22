46794720f200bfc9e1d55354e0bb0cade5a8b1f6
sametpek52 <samet.pektas@std.izu.edu.tr>
2015-12-22 13:14:50 +0200

    Hafta 3

--- note.m
+++ note.m
@@ -1,7 +1,22 @@
-function [xx,tt] = note(frek,orn,dur)
+function [xx,tt] = note(frek,dur,fs=8192)
     
-    %% "orn" periyot baþýna gerekli örnek sayýsý
+    %% "fs" periyot baþýna istenilen örnek sayýsý
 
-    tt = 0:1/(orn*frek):dur %% time
+    tt = 0:1/fs:(dur-1/fs); %% time
     
-    xx = sin(2*pi*frek*tt); %% sinüs sonuçlarý
+    %%zarf ta gerekli olanlar
+    elemanSayi = length(tt); %% Eleman sayýsý
+    
+    attack = linspace(0,1.5,(elemanSayi*2/8));
+    
+    decay = linspace(1.5-(0.5/(elemanSayi/8)),1,(elemanSayi/8));
+    
+    sustain = ones(1,(elemanSayi/2));
+    
+    relase = linspace(1-(1/(elemanSayi/8)),0,(elemanSayi/8));
+    
+    zarf = [attack, decay, sustain, relase];
+
+    xx = zarf .* sin(2*pi*frek*tt + zarf); %% zarf ile sinüs sonuçlarý
+
+    
